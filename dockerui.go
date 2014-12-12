980c850eaf22fd4fbeb8da0e60b92c6f79b3f3de
pastgift <pastgift@gmail.com>
2014-12-12 18:03:04 +0800

    Unfinished: Add login page

--- dockerui.go
+++ dockerui.go
@@ -10,13 +10,87 @@ import (
 	"net/url"
 	"os"
 	"strings"
+
+    "math/rand"
+    "time"
+    "strconv"
+    "fmt"
+    "crypto/md5"
 )
 
 var (
 	endpoint = flag.String("e", "/var/run/docker.sock", "Dockerd endpoint")
 	addr     = flag.String("p", ":9000", "Address and port to serve dockerui")
 	assets   = flag.String("a", ".", "Path to the assets")
+
+    username = ""
+    password = ""
+    token    = ""
 )
+
+func GetMD5(s string) string {
+    data := []byte(s)
+    return md5.Sum(data)
+}
+
+func CreateRandomString() string {
+    rand.Seed(time.Now().UnixNano())
+    randInt := rand.Int63()
+    randStr := strconv.FormatInt(randInt, 36)
+
+    return randStr
+}
+
+func AuthenticateUser(r *http.Request) bool {
+    cookie, err := r.Cookie("token")
+    if err != nil || cookie.Value == "" || cookie.Value != token {
+        fmt.Println("User not login")
+        return false
+    }
+    return true
+}
+
+type DoLoginHandler struct {
+}
+
+func (f *DoLoginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Inputed UserName:", r.PostFormValue("username"))
+    fmt.Println("Inputed Password:", r.PostFormValue("password"))
+    if r.Method != "POST" || r.PostFormValue("username") != username || GetMD5(r.PostFormValue("password")) != password {
+        http.Redirect(w, r, "/login.html", http.StatusFound)
+    }
+
+    token = CreateRandomString()
+
+    cookie := http.Cookie{
+        Name:"token",
+        Value:token,
+    }
+    http.SetCookie(w, &cookie)
+
+    http.Redirect(w, r, "/", http.StatusFound)
+}
+
+type AuthenticatedFileServer struct {
+    BaseFileServerHandler http.Handler
+}
+
+func NewAuthenticatedFileServer(root http.FileSystem) *AuthenticatedFileServer {
+    afs := AuthenticatedFileServer{}
+    afs.BaseFileServerHandler = http.FileServer(root)
+
+    return &afs
+}
+
+func (f *AuthenticatedFileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Request:", r.URL.Path)
+
+    if strings.HasPrefix(r.URL.Path, "/css/") || AuthenticateUser(r) == true {
+        f.BaseFileServerHandler.ServeHTTP(w, r)
+    } else {
+        http.Redirect(w, r, "/login.html", http.StatusFound)
+    }
+}
 
 type UnixHandler struct {
 	path string
@@ -69,7 +143,9 @@ func createUnixHandler(e string) http.Handler {
 func createHandler(dir string, e string) http.Handler {
 	var (
 		mux         = http.NewServeMux()
-		fileHandler = http.FileServer(http.Dir(dir))
+		loginHandler = http.FileServer(http.Dir(dir))
+        doLoginHandler = new(DoLoginHandler)
+        aFileHandler = NewAuthenticatedFileServer(http.Dir(dir))
 		h           http.Handler
 	)
 
@@ -85,13 +161,21 @@ func createHandler(dir string, e string) http.Handler {
 		h = createUnixHandler(e)
 	}
 
+    mux.Handle("/login.html", loginHandler)
+    mux.Handle("/dologin", doLoginHandler)
 	mux.Handle("/dockerapi/", http.StripPrefix("/dockerapi", h))
-	mux.Handle("/", fileHandler)
+	mux.Handle("/", aFileHandler)
 	return mux
+}
+
+func initUser() {
+    // TODO read user/password from shadow
+    token = ""
 }
 
 func main() {
 	flag.Parse()
+    initUser()
 
 	handler := createHandler(*assets, *endpoint)
 	if err := http.ListenAndServe(*addr, handler); err != nil {
